351b0728902ec965bec351df21e161a0f4dd7af5
Your Namelear <gheorghe.n.denismarian18@student.rau.ro>
2024-08-10 16:40:33 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>Software Developer</h2>
+  <a href="https://www.linkedin.com/in/denis-gheorghe/" class="link">
+    <img src="../assets/linkedin-logo.svg" class="logo" alt="linkedin logo" />
+   <h3>LinkedIn</h3>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
